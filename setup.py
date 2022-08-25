e6e6d5f45a4f897f4e5d645744cc49c36544c52d
KC Wong <kcwongaz@gmail.com>
2022-08-25 16:59:56 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="ust_wildfire",
-    version="1.0",
-    packages=find_packages("ust_wildfire")
+    version="1.0.0",
+    packages=find_packages("ust_wildfire"),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "pandas"
+    ],
+    extras_requires={
+        "ERA5": ["netCDF4"]
+    }
 )
